0cbf800c2687301410cc15bb906b2246c5166795
mkamiishi <mkamiishi@infragistics.com>
2023-12-27 14:25:32 +0900

    Initial commit
    初回コミット

---
+++ XamDataGrid_TextTrim/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Infragistics.Windows;
+using Infragistics.Windows.DataPresenter;
+using Infragistics.Windows.Editors;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamDataGrid_TextTrim
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
